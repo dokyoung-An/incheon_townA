6a021b0d6b409c5c3b46bb7dd187429d18d9fea7
Dokyung An <tea-co@naver.com>
2024-05-01 08:09:36 +0900

    수정8

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 7.0.10/20025
 // Filename: ?? ????? A??.ggsk
-// Generated 2024-04-22T15:48:35
+// Generated 2024-05-01T08:07:41
 
 function pano2vrSkin(player,base) {
 	player.addVariable('vis_roomchoice', 2, false, { ignoreInState: 0  });
